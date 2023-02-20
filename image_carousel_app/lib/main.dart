ec47c831bc9dcf8985a943f378be78077970be88
BDjaekwanee <jaekwanee@gmail.com>
2023-02-20 17:38:49 +0900

    feat : Create image carousel
    Close #5

--- image_carousel_app/lib/main.dart
+++ image_carousel_app/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:image_carousel_app/screen/home_screen.dart';
 
 void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
 }
-
